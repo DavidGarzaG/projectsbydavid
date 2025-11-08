71f7f07a99d58de04eb7d8a0227f1e43bc855121
DavidGarzaG <dagago97@gmail.com>
2025-11-07 18:01:46 -0600

    Hello World

---
+++ website/projects/home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import os
+
+st.header("Welcome to Hire David!")
+
+with st.container(border = True):
+
+    st.write(
+        """
+        This website is a portfolio of some of my projects in different fields of Data Science. 
+        Each of the projects serves to demonstrate my skills and expertise.
+        """)
+    
+st.subheader("Here is my latest Resume")
+st.pdf(os.path.join(os.getcwd(), "static", "Resume.pdf"), height=830)
+
+st.image(os.path.join(os.getcwd(), "static", "Brandy.JPG"), width=300)
